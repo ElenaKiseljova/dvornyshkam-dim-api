5e3d4e1c5529fa0f94f9cf835363f60c0984907d
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-18 15:19:20 +0200

    upd: api routes ([AnimalController::class, 'index'])

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\AnimalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/animals', [AnimalController::class, 'index']);
