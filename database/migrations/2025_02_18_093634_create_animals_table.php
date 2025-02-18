4172468711d8ab9d63652272ce7f279c7b33e380
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-18 15:16:09 +0200

    add: create_animals_table

---
+++ database/migrations/2025_02_18_093634_create_animals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Animal;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->enum('category', Animal::CATEGORIES);
+            $table->enum('gender', Animal::GENDERS);
+            $table->float('weight')->nullable()->comment('NULL: Not set. Number - weight animal in KG');
+            $table->date('birthday');
+            $table->text('image')->comment('Main image URL');
+            $table->text('images')->comment('An array of images URLs, joined by ", "');
+            $table->boolean('animal_friendly')->nullable()->default(null)->comment('NULL: Not set, FALSE: Not animal friendly, TRUE: Animal friendly');
+            $table->boolean('vaccinated')->nullable()->default(null)->comment('NULL: Not set, FALSE: Not vaccinated, TRUE: Vaccinated');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
