370271c5affe1e806ad44503e88c2962bc6ad3b6
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-20 17:00:34 +0200

    add: admin animals routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\AnimalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/animals', AnimalController::class);
+Route::delete('/animals/{animal}/restore', [AnimalController::class, 'restore'])
+    ->name('animals.restore')
+    ->withTrashed();
+Route::delete('/animals/{animal}/force-delete', [AnimalController::class, 'forceDelete'])
+    ->name('animals.force-delete')
+    ->withTrashed();
