be70d0b0ba1b635224aa7ec516364eedeb5baf8d
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-20 17:00:49 +0200

    upd: api animals routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AnimalController;
+use App\Http\Middleware\ApiMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/animals', [AnimalController::class, 'index']);
+Route::middleware(ApiMiddleware::class)->group(function () {
+    Route::get('/animals', [AnimalController::class, 'index']);
+    Route::get('/animals/{id}', [AnimalController::class, 'show']);
+});
