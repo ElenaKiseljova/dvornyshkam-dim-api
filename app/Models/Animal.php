6e563244a1ae26d9e3454a335f9b89e82d4b6e38
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-20 17:02:25 +0200

    upd: add SoftDeletes scope to the Animal model

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -6,11 +6,12 @@ use App\Models\Scopes\AllowedFilterSearch;
 use App\Models\Scopes\AllowedSort;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Animal extends Model
 {
 
-    use HasFactory, AllowedSort, AllowedFilterSearch;
+    use HasFactory, SoftDeletes, AllowedSort, AllowedFilterSearch;
 
     /**
      * The attributes that are mass assignable.
